e1989d4c5015e6d84be2e8e25201ef1898a68a10
s009hell <s009hell@gmail.com>
2018-10-01 12:31:49 +0700

    Добавление класса Rectangle.

---
+++ Shapes/src/Work/Rectangle.java
@@ -0,0 +1,36 @@
+package Work;
+
+public class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + height) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Высота = " + getWidth() + ", ширина = " + getHeight() + ", площадь = " + getArea() + ", периметр = " + getPerimeter() + ".";
+    }
+}
