1cea1fd9d22108ecf334fcaedabded451cbcff25
s009hell <s009hell@gmail.com>
2018-10-01 16:01:14 +0700

    Создание класса SortByArea.

--- Shapes/src/Demo/Main.java
+++ Shapes/src/Demo/Main.java
@@ -1,26 +1,33 @@
 package Demo;
 
-import Work.Circle;
-import Work.Rectangle;
-import Work.Square;
-import Work.Triangle;
+import Work.*;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Square square1 = new Square(7.0);
-        Square square2 = new Square(2.0);
-        Square square3 = new Square(11.0);
+        Shape shapesArray[] = new Shape[12];
 
-        Triangle triangle1 = new Triangle(0.0, 0.0, 1.0, 2.0, 0.0, 1.0);
-        Triangle triangle2 = new Triangle(11.0, 2.0, 1.0, 2.0, 7.0, 6.0);
-        Triangle triangle3 = new Triangle(0.0, 0.0, 5.0, 4.0, 1.0, 11.0);
+        shapesArray[0] = new Square(7.0);
+        shapesArray[1] = new Square(2.0);
+        shapesArray[2] = new Square(11.0);
 
-        Rectangle rectangle1 = new Rectangle(2, 3);
-        Rectangle rectangle2 = new Rectangle(10, 11);
-        Rectangle rectangle3 = new Rectangle(1, 2);
+        shapesArray[3] = new Triangle(0.0, 0.0, 1.0, 2.0, 0.0, 1.0);
+        shapesArray[4] = new Triangle(11.0, 2.0, 1.0, 2.0, 7.0, 6.0);
+        shapesArray[5] = new Triangle(0.0, 0.0, 5.0, 4.0, 1.0, 11.0);
 
-        Circle circle1 = new Circle(15);
-        Circle circle2 = new Circle(3);
-        Circle circle3 = new Circle(7);
+        shapesArray[6] = new Rectangle(2, 3);
+        shapesArray[7] = new Rectangle(10, 11);
+        shapesArray[8] = new Rectangle(1, 2);
+
+        shapesArray[9] = new Circle(15);
+        shapesArray[10] = new Circle(3);
+        shapesArray[11] = new Circle(7);
+
+        Arrays.sort(shapesArray, new SortByArea());
+
+        for (Shape x : shapesArray) {
+            System.out.println(x);
+        }
     }
 }
---
+++ Shapes/src/Work/SortByArea.java
@@ -0,0 +1,10 @@
+package Work;
+
+import java.util.Comparator;
+
+public class SortByArea implements Comparator<Shape> {
+    @Override
+    public int compare(Shape o1, Shape o2) {
+        return Double.compare(o1.getArea(), o2.getArea());
+    }
+}
