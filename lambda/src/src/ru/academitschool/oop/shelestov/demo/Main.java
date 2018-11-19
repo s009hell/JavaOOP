c6eb7e2679e82a55f7dec15fc760a77e940562ff
s009hell <s009hell@gmail.com>
2018-11-19 10:59:30 +0700

    Реализация lambda.

---
+++ lambda/src/src/ru/academitschool/oop/shelestov/demo/Main.java
@@ -0,0 +1,22 @@
+package src.ru.academitschool.oop.shelestov.demo;
+
+import src.ru.academitschool.oop.shelestov.work.Person;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Евгений", 12);
+        Person p2 = new Person("Евгений", 21);
+        Person p3 = new Person("Евгений", 49);
+        Person p4 = new Person("Иван", 14);
+        Person p5 = new Person("Иван", 17);
+        Person p6 = new Person("Петр", 24);
+        Person p7 = new Person("Петр", 66);
+        Person p8 = new Person("Максим", 16);
+        Person p9 = new Person("Владимир", 45);
+        Person p10 = new Person("Дмитрий", 51);
+
+    }
+}
---
+++ lambda/src/src/ru/academitschool/oop/shelestov/work/Person.java
@@ -0,0 +1,19 @@
+package src.ru.academitschool.oop.shelestov.work;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
