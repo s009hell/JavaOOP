a0532cd75893cdce4b7f2f0a86e7af3bcbf286f1
s009hell <s009hell@gmail.com>
2018-11-20 08:53:34 +0700

    Реализация lambda.

---
+++ Lambda/src/ru/academitschool/oop/shelestov/work/Person.java
@@ -0,0 +1,24 @@
+package ru.academitschool.oop.shelestov.work;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return ("[Имя: " + name + ", возраст: " + age +"]");
+    }
+}
